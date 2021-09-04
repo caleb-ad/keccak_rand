bc0fad9b972578d55a15916753f2912e7a36e6c4
Caleb Dennis <caleb.dennis429@gmail.com>
2021-09-03 18:32:50 -0600

    switched to use VLA as internal state

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod bit_stream;
+mod vla;
 
 use std::{
     vec::Vec,
@@ -8,80 +9,112 @@ use std::{
         Error,
         Display,
     },
-    convert::TryInto,
     cmp::min,
 };
 
 pub use bit_stream::BitStream;
+use vla::VLA;
 
 type Bit = u8;
 
 #[derive(PartialEq)]
-pub struct Keccak{
-    state: Vec<Vec<Vec<Bit>>>,
+pub struct Keccak<'a>{
+    state: &'a mut VLA<Bit>,
+    state1: &'a mut VLA<Bit>,
     w: usize, //depth, or length of each lane
     l: u64 //log base 2 of w
 }
 
-impl Keccak{
+macro_rules! mut_ref_swap {
+    ($first:expr, $second:expr, $T:ty) => {
+        unsafe{
+            let ptr1:*mut $T = ($first);
+            $first = &mut *($second as *mut $T);
+            $second = &mut *ptr1;
+        }
+    };
+}
+
+impl Keccak<'_>{
 
     /// 0<=x<=width, 0<=y<=height, 0<=z<=depth
-    pub fn get(& self, x: usize, y: usize, z: usize)->Bit{
-        return self.state[x][y][z];
+    /// gets from state
+    #[inline]
+    fn get(& self, x: usize, y: usize, z: usize)->Bit{
+        return self.state.get(self.w * (5*y + x) + z);
     }
 
-    pub fn get_lane(& self, x: usize, y: usize) -> & Vec<Bit>{
-        return &self.state[x][y];
+    /// sets state1
+    #[inline]
+    fn set(&mut self, x: usize, y: usize, z: usize, val: Bit){
+        self.state1.set(self.w * (5*y + x) + z, val);
+    }
+
+    #[inline]
+    fn set_state(&mut self, x: usize, y: usize, z: usize, val: Bit){
+        self.state.set(self.w * (5*y + x) + z, val);
+    }
+
+    #[inline]
+    fn get_idx(& self, x: usize, y: usize, z: usize)->usize{
+        return self.w * (5*y + x) + z;
+    }
+
+    pub fn get_lane(& self, x: usize, y: usize) -> Vec<Bit>{
+        let mut temp = Vec::new();
+        for z in 0..self.w{
+            temp.push(self.get(x,y,z));
+        }
+        return temp;
     }
 
     pub fn depth(& self) -> usize{
         return self.w;
     }
 
-    pub fn set(&mut self, x: usize, y: usize, z: usize, val: Bit){
-        self.state[x][y][z] = val;
-    }
-
     /// state arrays depth initialized to message length / 25.
     /// If the message is not evenly divisble by 25 then the message size will
-    /// be rounded down to the nearest multiple of 25,
-    /// excess in message is unused
+    /// be rounded down to the nearest multiple of 25, if message contains more
+    /// bits than state then some bits in message are unused
     pub fn new(message: & BitStream) -> Self{
+        unsafe{
         let mut temp = Keccak{
-            state: Vec::new(),
-            w: (message.len() as i64 / 25).try_into().unwrap(),
+            state: &mut *(Box::into_raw(Box::from(VLA::new((message.len() / 25) * 25)))),
+            state1: &mut *(Box::into_raw(Box::from(VLA::new((message.len() / 25) * 25)))),
+            w: message.len() / 25,
             l: 0
         };
-        temp.l = f64::log2(temp.w as f64) as u64;
-        temp.state = temp.empty_state();
+        temp.l = f64::log2(temp.w as f64) as u64; //w should equal 2^(integer)
         for idx in 0..min(message.len(), temp.w * 25){
-            temp.set((idx / temp.w) % 5, idx / (temp.w * 5), idx % temp.w, message[idx]);
+            temp.state.set(idx, message[idx]);
         }
         return temp;
+        }
     }
 
     /// behaves like new, except the state depth is set to size,
     /// if message contains fewer bit then the state, the remaining bits are
-    /// zero-initialized. if message contains more bits than state the, some
-    /// bits in message are unused
-    pub fn new_sized(message: & BitStream, size: u64) -> Self{
+    /// zero-initialized.
+    pub fn new_sized(message: & BitStream, size: usize) -> Self{
+        unsafe{
         let mut temp = Keccak{
-            state: Vec::new(),
-            w: size as usize,
+            state: &mut *(Box::into_raw(Box::from(VLA::new(size*25)))),
+            state1: &mut *(Box::into_raw(Box::from(VLA::new(size*25)))),
+            w: size,
             l: 0
         };
         temp.l = f64::log2(temp.w as f64) as u64;
-        temp.state = temp.empty_state();
         for idx in 0..min(message.len(), temp.w * 25){
-            temp.set((idx / temp.w) % 5, idx / (temp.w * 5), idx % temp.w, message[idx]);
+            temp.state.set(idx, message[idx]);
         }
         return temp;
+        }
     }
 
     pub fn get_state(& self) -> BitStream{
         let mut tmp = BitStream::new(self.w * 25);
         for idx in 0..(25 * self.w){
-            tmp.set(idx, self.get((idx / self.w) % 5, idx / (self.w * 5), idx % self.w));
+            tmp.set(idx, self.state.get(idx));
         }
         return tmp;
     }
@@ -90,80 +123,64 @@ impl Keccak{
     pub fn copy_to_u64(& self) -> u64{
         let mut temp:u64 = 0;
         for idx in 0..64{
-            temp |= (self.get(idx / self.w % 5, idx / (self.w * 5), idx % self.w) as u64) << (63 - idx);
+            temp |= (self.state.get(idx) as u64) << (63 - idx);
         }
         return temp;
     }
 
-    pub fn empty_state(&self) -> Vec<Vec<Vec<Bit>>>{
-        let mut temp = Vec::new();
-        temp.resize(5, Vec::new());
-        for idx in 0 .. 5 {
-            temp[idx].resize(5, Vec::new());
-            for idx1 in 0 .. 5 {
-                temp[idx][idx1].resize(self.w, 0);
-            }
-        }
-        return temp;
+    fn column_parity<'a>(&'a self, x: usize, z: usize) -> Bit{
+        return self.get(x,0,z) ^ self.get(x,1,z) ^ self.get(x,2,z)
+            ^ self.get(x,3,z) ^ self.get(x,4,z);
     }
 
-    fn column_parity(&self, x: usize, z: usize) -> Bit{
-        return self.state[x][0][z] ^ self.state[x][1][z] ^ self.state[x][2][z]
-            ^ self.state[x][3][z] ^ self.state[x][4][z];
-    }
-
-    fn theta(&mut self){
-        let mut new_state = self.empty_state();
+    fn theta<'a>(&'a mut self){
         for x in 0..(5 as i64){
             for z in 0..(self.w as i64){
-                let d = self.column_parity((x-1) as usize %5, z as usize) ^
-                    self.column_parity((x+1) as usize %5, (z-1) as usize %(self.w));
+                let d = self.column_parity(((x-1) % 5).abs() as usize, z as usize) ^
+                    self.column_parity((x+1) as usize % 5, ((z-1) % self.w as i64).abs() as usize);
                 for y in 0..(5 as usize){
-                    new_state[x as usize][y][z as usize] = self.state[x as usize][y][z as usize] ^ d;
+                    self.set(x as usize, y, z as usize, self.get(x as usize, y, z as usize) ^ d);
                 }
             }
         }
-        self.state = new_state;
+        mut_ref_swap!(self.state, self.state1, VLA<Bit>);
     }
 
-    fn rho(&mut self){
-        let mut new_state = self.empty_state();
-        for z in 0..self.w { new_state[0][0][z] = self.state[0][0][z] }
+    fn rho<'a>(&'a mut self){
+        for z in 0..self.w { self.set(0, 0, z, self.get(0,0,z)) }
         // could use lookup table to reduce calculations
         let (mut x, mut y) = (1, 0);
         for t in 0..23{
-            for z in 0..(self.w as i64){
-                new_state[x][y][z as usize] = self.state[x][y][(z - (t+1)*(t+2)/2) as usize % self.w];
+            for z in 0..self.w{
+                self.set(x, y, z, self.get(x, y, (z - (t+1)*(t+2)/2) as usize % self.w ));
             }
             let temp = y;
             y = (2*x + 3*y) % 5;
             x = temp;
         }
-        self.state = new_state;
+        mut_ref_swap!(self.state, self.state1, VLA<Bit>);
     }
 
-    fn pi(&mut self){
-        let mut new_state = self.empty_state();
+    fn pi<'a>(&'a mut self){
         for x in 0..(5 as usize){
             for y in 0..(5 as usize){
                 for z in 0..self.w{
-                    new_state[x][y][z] = self.state[(x + 3*y) % 5][x][z];
+                    self.state1.set(self.get_idx(x, y, z), self.get((x + 3*y) % 5, x, z));
                 }
             }
         }
-        self.state = new_state;
+        mut_ref_swap!(self.state, self.state1, VLA<Bit>);
     }
 
-    fn chi(&mut self){
-        let mut new_state = self.empty_state();
+    fn chi<'a>(&'a mut self){
         for x in 0..(5 as usize){
             for y in 0..(5 as usize){
                 for z in 0..self.w{
-                    new_state[x][y][z] = self.state[x][y][z] ^ ((self.state[(x+1)%5][y][z] ^ 1) & self.state[(x+2)%5][y][z]);
+                    self.set(x, y, z, self.get(x,y,z) ^ ((self.get((x+1) % 5, y, z) ^ 1) & self.get((x+2) % 5, y, z)));
                 }
             }
         }
-        self.state = new_state;
+        mut_ref_swap!(self.state, self.state1, VLA<Bit>);
     }
 
     fn rc(t: u64) -> Bit{
@@ -181,17 +198,17 @@ impl Keccak{
         return (state as u8) & 1;
     }
 
-    fn iota(&mut self, round: u64){
+    fn iota(& mut self, round: u64){
         let mut rc = BitStream::new(self.w);
         for j in 0..self.l{
             rc.set(i64::pow(2, j as u32) as usize - 1, Keccak::rc(j as u64 + 7*round))
         }
         for z in 0..self.w{
-            self.state[0][0][z] ^= rc.get(z);
+            self.set_state(0, 0, z, self.get(0,0,z) ^ rc.get(z));
         }
     }
 
-    pub fn keccak(&mut self, num_rounds: u64){
+    pub fn keccak(& mut self, num_rounds: u64){
         for r in (12 + 2 * self.l - num_rounds as u64) .. (12 + 2 * self.l){
             self.theta();
             self.rho();
@@ -204,7 +221,7 @@ impl Keccak{
     /// pads message so it may be split evenly into blocks,
     /// each block is xor'ed with the current state, and a round of keccak is done
     /// unoptimized: likely slow
-    pub fn sponge_absorb(&mut self, message:&mut BitStream){
+    pub fn sponge_absorb(& mut self, message:&mut BitStream){
         if message.len() % (self.w * 25) != 0{
             let pad = (f64::ceil(message.len() as f64 / (self.w * 25) as f64) as u64 * (self.w * 25) as u64) - message.len() as u64;
             let mut temp = Vec::<u8>::new();
@@ -217,7 +234,7 @@ impl Keccak{
             for x in 0..5 as usize{
                 for y in 0..5 as usize{
                     for z in 0..self.depth(){
-                        self.set(x, y, z, self.get(x,y,z) ^ message.get(self.depth() * (5*y + x) + z));
+                        self.set_state(x, y, z, self.get(x,y,z) ^ message.get(self.depth() * (5*y + x) + z));
                     }
                 }
             }
@@ -227,7 +244,7 @@ impl Keccak{
     }
 }
 
-impl Debug for Keccak{
+impl Debug for Keccak<'_>{
     fn fmt(& self, f: &mut Formatter<'_>) -> Result<(), Error>{
         for idx in 0..25{
             match write!(f, "x:{} y:{}, {:?}\n", idx % 5, idx / 5, self.get_lane(idx % 5, idx / 5)){
@@ -240,10 +257,19 @@ impl Debug for Keccak{
 }
 
 /// allows use of ToString
-impl Display for Keccak{
+impl Display for Keccak<'_>{
     fn fmt(& self, f: &mut Formatter<'_>) -> Result<(), Error>{
         return Debug::fmt(self, f);
     }
+}
+
+impl Drop for Keccak<'_>{
+    fn drop(&mut self){ unsafe {
+        let ptr1:*mut VLA<Bit> = self.state;
+        let ptr2:*mut VLA<Bit> = self.state1;
+        std::ptr::drop_in_place(ptr1);
+        std::ptr::drop_in_place(ptr2);
+    }}
 }
 
 #[cfg(test)]
@@ -251,7 +277,27 @@ mod tests {
 
     use crate::Keccak;
     use crate::bit_stream::BitStream;
+    use crate::vla::VLA;
     use std::mem::size_of;
+
+    #[test]
+    fn test_vla() {
+        let mut a = VLA::<u64>::new(10);
+        for x in 0..10{
+            assert_eq!(a.len(), 10);
+            assert_eq!(a.get(x), 0);
+            a.set(x, x as u64);
+            assert_eq!(a.get(x), x as u64);
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_vla_panic_get(){
+        let mut a = VLA::<u64>::new(5);
+        a.get(5);
+        a.set(5, 1);
+    }
 
     #[test]
     fn test_bitstream_get() {
@@ -331,8 +377,8 @@ mod tests {
     fn test_keccak_new() {
         // create 200 bit, 5*5*8 state array
         let state = Keccak::new(& BitStream::from_str("abcdefghijklmnopqrstuvwxy"));
-        assert_eq!(*state.get_lane(0, 0), vec!(0,1,1,0,0,0,0,1));
-        assert_eq!(*state.get_lane(4, 4), vec!(0,1,1,1,1,0,0,1))
+        assert_eq!(state.get_lane(0, 0), vec!(0,1,1,0,0,0,0,1));
+        assert_eq!(state.get_lane(4, 4), vec!(0,1,1,1,1,0,0,1))
     }
 
     #[test]
--- tests/randomness.rs
+++ tests/randomness.rs
@@ -1,6 +1,5 @@
 use rand_keccak::Keccak;
 use rand_keccak::BitStream;
-use std::time::*;
 
 fn gen_sample(size: u32, range_max: u64) -> Vec<u64>{
    let mut temp = Vec::new();
